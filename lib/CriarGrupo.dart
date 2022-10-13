efc21637d38ed85a5de2f0386dacd156633e412f
Jonathan Moreira <jonathanf.fmoreira@gmail.com>
2022-10-13 03:08:27 -0300

    fazendo a tela de grupos funcionar com o firebase

--- lib/CriarGrupo.dart
+++ lib/CriarGrupo.dart
@@ -9,6 +9,8 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:progress_state_button/iconed_button.dart';
+import 'package:progress_state_button/progress_button.dart';
 
 
 class CriarGrupo extends StatefulWidget {
@@ -30,7 +32,9 @@ class _CriarGrupoState extends State<CriarGrupo> {
   final _url = TextEditingController();
   final _userDoc = TextEditingController();
   final _redeS = TextEditingController();
-
+  List<String> list = <String>['Whatsapp', 'Facebook'];
+  String selectedValue = "";
+  ButtonState stateTextWithIcon = ButtonState.idle;
 
   Future <void> inicializarFirebase() async {
     await Firebase.initializeApp();
@@ -95,8 +99,45 @@ class _CriarGrupoState extends State<CriarGrupo> {
                 InputText(Color.fromARGB(255,167,196,254), "Nome do Grupo", false, controller: _nome),
                 SizedBox(height: size.height*0.02,),
                 InputText(Color.fromARGB(255,167,196,254), "Link do Grupo", false, controller: _url),
-                SizedBox(height: size.height*0.02,),
-                InputText(Color.fromARGB(255,167,196,254), "Rede social (Facebook ou Whatsapp)", false, controller: _redeS),
+                SizedBox(height: size.height*0.05,),
+
+                Container(
+                  width: 350.0,
+                  child: ButtonTheme(
+                    alignedDropdown: true,
+                    child: DropdownButtonFormField<String>(
+                      items: list.map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value.toLowerCase(),
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+                        labelStyle: TextStyle(
+                          color: Color.fromARGB(255,167,196,254),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                          borderSide: BorderSide(
+                            color:Color.fromARGB(255, 67, 67, 67),
+                          )
+                        ),
+                        
+                      ),
+                      onChanged: (String? newValue){
+                        setState(() {
+                          selectedValue = newValue!;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+
+                // InputText(Color.fromARGB(255,167,196,254), "Rede social (Facebook ou Whatsapp)", false, controller: _redeS),
                 SizedBox(height: size.height*0.02,),
 
 
@@ -104,16 +145,40 @@ class _CriarGrupoState extends State<CriarGrupo> {
                   width: 180,
                   height: 70,
                   padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                    primary: Color.fromARGB(255,167,196,254), //Cor do botão
-                    onPrimary: Colors.white, //Cor do trexto dentro do botão
-                    shape: RoundedRectangleBorder( // Deixando o botão circular
-                      borderRadius: BorderRadius.circular(50), // <-- Radius
-                    ),),
-                  child: Text('Cadastrar'),
-                  onPressed: () {addDados(context);},
+                  child: ProgressButton.icon(iconedButtons: {
+                    ButtonState.idle: IconedButton(
+                      color: Color.fromARGB(255,167,196,254),
+                      text: "Cadastrar",
+                    ),
+                    ButtonState.loading: IconedButton(
+                      icon: Icon(Icons.send, color: Colors.white),
+                      color: Color.fromARGB(255,167,196,254),
+                      text: "Carregando",
+                    ),
+                    ButtonState.fail: IconedButton(
+                      color: Color.fromARGB(255,167,196,254),
+                      text: "Cadastrar",
+                    ),
+                    ButtonState.success: IconedButton(
+                      text: "Sucesso",
+                      color: Colors.green.shade400
+                    ),},
+                    onPressed: () {
+                      addDados(context);
+                    },
+                    state: stateTextWithIcon,
                   ),
+                  
+                  // child: ElevatedButton(
+                  //   style: ElevatedButton.styleFrom(
+                  //   primary: Color.fromARGB(255,167,196,254), //Cor do botão
+                  //   onPrimary: Colors.white, //Cor do trexto dentro do botão
+                  //   shape: RoundedRectangleBorder( // Deixando o botão circular
+                  //     borderRadius: BorderRadius.circular(50), // <-- Radius
+                  //   ),),
+                  // child: Text('Cadastrar'),
+                  // onPressed: () {addDados(context);},
+                  // ),
                 ),
               ],
             )
@@ -126,15 +191,17 @@ class _CriarGrupoState extends State<CriarGrupo> {
     );
   }    
   void addDados(BuildContext ctx){    
+    setState(() {
+      stateTextWithIcon = ButtonState.loading;
+    });
     Grupo l = new Grupo();
     l.nome = _nome.text.toString().trim();
     l.url = _url.text.toString().trim();
     l.user_doc = widget.email.trim();
-    l.rede = _redeS.text.toString().trim();
+    l.rede = selectedValue.trim();
     l.dt = DateTime.now();
-    print(_redeS.text.toUpperCase());
 
-    if (_redeS.text.toUpperCase().trim() == "FACEBOOK" || _redeS.text.toUpperCase() == "WHATSAPP") {
+    if (l.rede.toUpperCase().trim() == "FACEBOOK" || l.rede.toUpperCase() == "WHATSAPP") {
       CollectionReference instanciaColecao = FirebaseFirestore.instance.collection("groups");
       Future <void> addGroup(){
         return instanciaColecao
@@ -143,19 +210,25 @@ class _CriarGrupoState extends State<CriarGrupo> {
         .then((value) {
           print("Grupo adicionado");
           _abreOutraTela(ctx, Grupos());
-            ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: ErrorMessage("Sucesso","Grupo cadastrado com sucesso!", Colors.green),
-              behavior: SnackBarBehavior.floating,
-              backgroundColor: Colors.transparent,
-              margin: EdgeInsets.only(bottom: 615),
-              elevation: 0,
-            ),
+          setState(() {
+            stateTextWithIcon = ButtonState.idle;
+          });
+          ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: ErrorMessage("Sucesso","Grupo cadastrado com sucesso!", Colors.green),
+            behavior: SnackBarBehavior.floating,
+            backgroundColor: Colors.transparent,
+            margin: EdgeInsets.only(bottom: 615),
+            elevation: 0,
+          ),
           );
         }
         )
         .catchError((onError){
           print("Error ${onError.toString()}");
+          setState(() {
+            stateTextWithIcon = ButtonState.idle;
+          });
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(
               content: ErrorMessage("Erro",onError.toString(), Colors.red),
@@ -169,9 +242,12 @@ class _CriarGrupoState extends State<CriarGrupo> {
       }
       addGroup();
     }else{
+      setState(() {
+        stateTextWithIcon = ButtonState.idle;
+      });
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: ErrorMessage("Erro!","Digite 'Facebook' ou 'Whatsapp' para identificar a rede.", Colors.red),
+          content: ErrorMessage("Erro!","Selecione uma das redes sociais para cadastrar o grupo", Colors.red),
           behavior: SnackBarBehavior.floating,
           backgroundColor: Colors.transparent,
           margin: EdgeInsets.only(bottom: 615),
--- lib/Grupos.dart
+++ lib/Grupos.dart
@@ -4,12 +4,14 @@ import 'package:duda/CriarGrupo.dart';
 import 'package:duda/Loading02.dart';
 import 'package:duda/Login.dart';
 import 'package:duda/Mapa.dart';
+import 'package:duda/widget/error_message.dart';
 import 'package:duda/widget/input_text.dart';
 import 'package:duda/widget/menu.dart';
 import 'package:duda/widget/modal_bloqueio.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 
@@ -25,6 +27,7 @@ class _Grupos extends State<Grupos> {
   String _uname = '';
   String _email = '';
   bool anonimo = false;
+  int grupos = 0;
 
   late FirebaseAuth _auth;
 
@@ -45,13 +48,11 @@ class _Grupos extends State<Grupos> {
     }
   }
 
-  _getGroups () async {
-    QuerySnapshot grupos = await FirebaseFirestore.instance.collection('groups').get();
-    List docs = grupos.docs;
-    int _qtdGroups = docs.length;
-    print(docs.length);
-    docs.forEach((doc){
-      //print(doc['nome']);
+  Future _getGroups () async {
+    QuerySnapshot data = await FirebaseFirestore.instance.collection('groups').get();
+    setState(() {
+      grupos = data.docs.length;
+
     });
   }
 
@@ -68,153 +69,152 @@ class _Grupos extends State<Grupos> {
       drawer: Drawer(
         child: MenuLateral(_uname, _email, anonimo),
       ),
-      body: _telaGrupos(),
+      body: _tela(),
     );
   }
-  _telaGrupos(){
+  _tela(){
     Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: SingleChildScrollView(
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Container(
-            height: size.height* 0.25,
-            color: Color.fromARGB(255,167,196,254),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                //Icon(Icons.group, color: Colors.white, size: 30,),
-                //SizedBox(height: size.height*0.01),
-                Text('Grupos', style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: "Mandali",
-                    fontSize: 50,
-                    fontWeight: FontWeight.w400,
-                    
-                  ),),
-              ],
-            )
-          ),
-          Container(
-            height: size.height*0.025,
-            transform: Matrix4.translationValues(0.0, -20.0, 0.0),            
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30), 
-                topRight: Radius.circular(30), 
-                bottomLeft: Radius.zero,
-                bottomRight: Radius.zero,
-              ),
-              color: Colors.white,
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('groups').orderBy('dt', descending: true).snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        return Column(
+          children: [
+            Container(
+              height: 180,
+              color: Color.fromARGB(255,167,196,254),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //Icon(Icons.group, color: Colors.white, size: 30,),
+                  //SizedBox(height: size.height*0.01),
+                  Text('Grupos', style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: "Mandali",
+                      fontSize: 50,
+                      fontWeight: FontWeight.w400,
+                      
+                    ),),
+                ],
+              )
             ),
-          ),
-          Container(
-            height: size.width,
-            width: size.width,
-            transform: Matrix4.translationValues(0.0, -20.0, 0.0),            
-            color: Colors.white,
-            child: Column(
-              children: [
-                IconButton(
-                  tooltip: "Adicione um grupo",
-                  onPressed: (){_abreOutraTela(context, CriarGrupo(_uname, _email, anonimo));}, 
-                  icon: Icon(
-                    Icons.add_circle_rounded, 
-                    color: Color.fromARGB(255, 140, 165, 214), 
-                    size: 30.0,
-                    shadows: [
-                      BoxShadow(
-                        color: Color.fromARGB(150, 158, 158, 158),
-                        spreadRadius: 4,
-                        blurRadius: 10,
-                        offset: Offset(0, 2),
-
-                      )
-                    ],
-                  ),
-                  padding: EdgeInsets.fromLTRB(300, 0, 0, 0),
+            Container(
+              height: size.height*0.025,
+              transform: Matrix4.translationValues(0.0, -20.0, 0.0),            
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30), 
+                  topRight: Radius.circular(30), 
+                  bottomLeft: Radius.zero,
+                  bottomRight: Radius.zero,
                 ),
-                
-                Container(
-                  height: 60,
-                  margin: EdgeInsets.only(left: 28, right: 30, top: 20),
-                  width: size.width,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color.fromARGB(101, 33, 149, 243),
-                        spreadRadius: 1,
-                        blurRadius: 5,
-                        offset: Offset(0, 3),
-                      )
-                    ],
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: 
-                          Padding(
-                            padding: const EdgeInsets.only(left: 50),
-                            child: Text("Nome do grupo"),
-                          ),
-                      ),
-
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10, bottom: 6),
-                        child: IconButton(onPressed: (){}, icon: Icon(Icons.facebook,size: 40,color: Colors.blue,)),
-                      )
-                    ],
+                color: Colors.white,
+              ),
+            ),
+            Container(
+              height: 50,
+              width: size.width,
+              transform: Matrix4.translationValues(0.0, -20.0, 0.0),            
+              color: Colors.white,
+              child: Column(
+                children: [
+                  IconButton(
+                    tooltip: "Adicione um grupo",
+                    onPressed: (){_abreOutraTela(context, CriarGrupo(_uname, _email, anonimo));}, 
+                    icon: Icon(
+                      Icons.add_circle_rounded, 
+                      color: Color.fromARGB(255, 140, 165, 214), 
+                      size: 30.0,
+                      shadows: [
+                        BoxShadow(
+                          color: Color.fromARGB(150, 158, 158, 158),
+                          spreadRadius: 4,
+                          blurRadius: 10,
+                          offset: Offset(0, 2),
+    
+                        )
+                      ],
+                    ),
+                    padding: EdgeInsets.fromLTRB(300, 0, 0, 0),
                   ),
                   
-                ),
-                Container(
-                  height: 60,
-                  margin: EdgeInsets.only(left: 28, right: 30, top: 25),
-                  width: size.width,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color.fromARGB(100, 58, 243, 33),
-                        spreadRadius: 1,
-                        blurRadius: 5,
-                        offset: Offset(0, 3),
-                      )
-                    ],
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: 
-                          Padding(
-                            padding: const EdgeInsets.only(left: 50),
-                            child: Text("Nome do grupo"),
-                          ),
+                ],
+              )
+            ),
+            Expanded(
+              child: snapshot.hasData ? ListView(
+                physics: const AlwaysScrollableScrollPhysics(), // new
+                children: snapshot.data!.docs.map((e) {
+                  return Center(
+                    child: GestureDetector(
+                      onTap: () => {
+                        _launchInBrowser(Uri.parse(e['url']))
+                      },
+                      child: Container(
+                        height: 60,
+                        margin: EdgeInsets.only(left: 28, right: 30, top: 10, bottom: 20),
+                        width: size.width,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                              color: e['rede'] == "facebook" ? Color.fromARGB(101, 33, 149, 243) : Color.fromARGB(100, 58, 243, 33),
+                              spreadRadius: 1,
+                              blurRadius: 5,
+                              offset: Offset(0, 3),
+                            )
+                          ],
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: 
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 50),
+                                  child: Text(e['nome']),
+                                ),
+                            ),
+                          
+                            Padding(
+                              padding: const EdgeInsets.only(right: 10, bottom: 6),
+                              child: e['rede'] == "facebook" ? 
+                                IconButton(onPressed: (){}, icon: Icon(Icons.facebook,size: 40,color: Colors.blue,)) : 
+                                IconButton(onPressed: (){}, icon: Icon(Icons.whatsapp,size: 40,color: Colors.green,)),
+                            ),
+                    
+                          ],
+                        ),
+                        
                       ),
-
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10, bottom: 6),
-                        child: IconButton(onPressed: (){}, icon: Icon(Icons.whatsapp,size: 40,color: Colors.green,)),
-                      )
-                    ],
-                  ),
-                  
-                )
-
-              ],
-            )
-          ),
-              
-        ],
-
-      ),
-    ),
+                    ));
+                  }).toList(),
+              ) : Text(
+                "Nenhum grupo encontrado!"
+              ),
+            ),
+          ],
+    
+        );
+      }
     );
+  }
+
+  Future<void> _launchInBrowser(Uri url) async {
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.externalApplication,
+    )) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: ErrorMessage("Erro","Erro ao abrir o link!", Colors.red),
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: Colors.transparent,
+          margin: EdgeInsets.only(bottom: 615),
+          elevation: 0,
+        ),
+      );
+    }
   }
 }
 

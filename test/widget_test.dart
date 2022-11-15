5b8977f9ed9c0257ed85d83b8a211a6fcd94d8f3
ErikSeabra <seabraerik@gmail.com>
2022-11-15 06:32:00 -0300

    teste

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:duda/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
